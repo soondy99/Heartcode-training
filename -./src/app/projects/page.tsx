76629888e637c234a00c120ccb3777903c4d0f4c
Soon MBA <soonmba@Soons-MacBook-Air.local>
2024-10-11 11:47:58 +0800

    hi

--- -./src/app/projects/page.tsx
+++ -./src/app/projects/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 import React from "react";
-import { calsans } from "@/fonts/calsans";
+
 import Image from "next/image";
 import { twMerge } from "tailwind-merge";
-import { TracingBeam } from "../ui/tracing-beam";
+import { TracingBeam } from "@/components/ui/tracing-beam";
 
-export function TracingBeamDemo() {
+
+export default function TracingBeamDemo() {
   return (
     <TracingBeam className="px-6">
       <div className="max-w-2xl mx-auto antialiased pt-4 relative">
@@ -44,31 +45,11 @@ const dummyContent = [
     description: (
       <>
         <p>
-          Sit duis est minim proident non nisi velit non consectetur. Esse
-          adipisicing laboris consectetur enim ipsum reprehenderit eu deserunt
-          Lorem ut aliqua anim do. Duis cupidatat qui irure cupidatat incididunt
-          incididunt enim magna id est qui sunt fugiat. Laboris do duis pariatur
-          fugiat Lorem aute sit ullamco. Qui deserunt non reprehenderit dolore
-          nisi velit exercitation Lorem qui do enim culpa. Aliqua eiusmod in
-          occaecat reprehenderit laborum nostrud fugiat voluptate do Lorem culpa
-          officia sint labore. Tempor consectetur excepteur ut fugiat veniam
-          commodo et labore dolore commodo pariatur.
-        </p>
-        <p>
-          Dolor minim irure ut Lorem proident. Ipsum do pariatur est ad ad
-          veniam in commodo id reprehenderit adipisicing. Proident duis
-          exercitation ad quis ex cupidatat cupidatat occaecat adipisicing.
-        </p>
-        <p>
-          Tempor quis dolor veniam quis dolor. Sit reprehenderit eiusmod
-          reprehenderit deserunt amet laborum consequat adipisicing officia qui
-          irure id sint adipisicing. Adipisicing fugiat aliqua nulla nostrud.
-          Amet culpa officia aliquip deserunt veniam deserunt officia
-          adipisicing aliquip proident officia sunt.
+        An avid runner, they find joy in the great outdoors, relishing the thrill of hitting the trails and exploring new landscapes. Each run is an adventure, a chance to connect with nature while pushing personal limits. Whether navigating serene forest paths or challenging mountain terrains, the experience invigorates both body and spirit.
         </p>
       </>
     ),
-    badge: "React",
+    badge: "About Me",
     image:
       "https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?auto=format&fit=crop&q=80&w=3540&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
   },
